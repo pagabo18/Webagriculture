8343c86cd73bb4b4aa59ece3b50c62eaded2c577
Francisco Sevilla <fsevilla@gmail.com>
2022-03-09 08:06:49 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'iteso-pae-2022-ui';
+}

59c1ab24ecbb707c0c83a0a8b891bef5ef8f2636
Francisco Sevilla <fsevilla@gmail.com>
2022-03-16 07:49:23 -0600

    Added core components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'iteso-pae-2022-ui';
+export class AppComponent implements OnInit {
+  name: string = 'Francisco';
+
+  movies: string[] = [
+    'Los tres Huastecos',
+    'Alien',
+    'Batman',
+    'Una pelicula de huevos',
+    'Pineapple express'
+  ];
+
+  flag: boolean = true;
+
+  constructor() {
+    setTimeout(() => {
+      this.name = 'Juan';
+      this.movies.push('Los 3 Garcia');
+    },2000);
+  }
+
+  doOnClick() {
+    console.log('me hicieron click!');
+    this.flag = !this.flag;
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
